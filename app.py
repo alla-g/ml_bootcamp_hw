dd99d7bfbfbbe90813a9657590772faebfabecb6
alla-g <43267989+alla-g@users.noreply.github.com>
2023-07-02 00:57:48 +0300

    finish app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-import time
 import streamlit as st
 import pandas as pd
 from model import open_data, preprocess_data, get_importances, predict_on_input
 from PIL import Image
 
-_, _, _, _, scaler = preprocess_data(open_data('data/clients.csv'))
+
+def preload_content():
+    """ preload content used in web app """
+
+    _, _, _, _, scaler = preprocess_data(open_data('data/clients.csv'))
+
+    plane = Image.open('data/plane.jpg')
+    age = Image.open('data/Age.png')
+    cat = Image.open('data/Categorical.png')
+    dist = Image.open('data/Distance.png')
+    delays = Image.open('data/Delays.png')
+    scores = Image.open('data/Scores.png')
+
+    return scaler, plane, age, cat, dist, delays, scores
 
 
 def highlight_weighs(s):
@@ -16,7 +28,7 @@ def highlight_weighs(s):
 def pack_input(sex, age, loyalty, distance, p_class, travel_type, dep_delay, arr_delay, wifi, fun,
                time_conv, onboard_service, booking, leg, gate_loc, baggage, food, checkin,
                online_boarding, inflight_service, seat, cleanliness):
-    """ translates input values to pass to model """
+    """ translate input values to pass to model """
 
     rule = {'Женский': 1,
             'Мужской': 0,
@@ -52,121 +64,133 @@ def pack_input(sex, age, loyalty, distance, p_class, travel_type, dep_delay, arr
     return pd.DataFrame(data, index=[0])
 
 
-st.set_page_config(layout="wide",
-                   page_title="Полёты и опросы",
-                   page_icon=':airplane:')
+def render_page(scaler, plane, age, cat, dist, delays, scores):
+    """ creates app page with tabs """
 
-st.title('Удовлетворённость полётом')
-st.subheader('Исследуем оценки, предсказываем удовлетворённость, оцениваем важность факторов')
-st.write('Материал - данные пассажиров и постполётные опросы')
+    st.title('Удовлетворённость полётом')
+    st.subheader('Исследуем оценки, предсказываем удовлетворённость, оцениваем важность факторов')
+    st.write('Материал - данные пассажиров и постполётные опросы')
+    st.image(plane)
 
-tab1, tab2, tab3 = st.tabs([':mag: Исследовать', ':mage: Предсказать', ':vertical_traffic_light: Оценить'])
+    tab1, tab2, tab3 = st.tabs([':mag: Исследовать', ':mage: Предсказать', ':vertical_traffic_light: Оценить'])
 
-with tab1:
-    st.write('Exploratory data analysis: исследуем наши данные, предварительно очищенные и обработанные :sparkles: :sparkles:')
+    with tab1:
+        st.write('Exploratory data analysis: исследуем наши данные, предварительно очищенные и обработанные :sparkles:')
 
-    st.write('**Возраст пассажиров**')
-    age = Image.open('data/Age.png')  # мб вынести наверх
-    st.image(age)
-    st.divider()
+        st.write('**Возраст пассажиров**')
+        st.image(age)
+        st.write('Самый распространённый возраст - от 20 до 60 лет')
+        st.divider()
 
-    st.write('**Категориальные признаки:** пол, лояльность, тип поездки и удовлетворённость')
-    cat = Image.open('data/Categorical.png')  # мб вынести наверх
-    st.image(cat)
-    st.divider()
+        st.write('**Категориальные признаки:** пол, лояльность, тип поездки и удовлетворённость')
+        st.image(cat)
+        st.write('Пассажиры примерно равно распределены по полу')
+        st.write('При этом лояльных и путешествующих по работе больше, чем нелояльных и путешествующих по личным нуждам')
+        st.write('Довольных меньше, чем нейтральных и недовольных')
+        st.divider()
 
-    st.write('**Расстояние полёта**')
-    dist = Image.open('data/Distance.png')  # мб вынести наверх
-    st.image(dist)
-    st.divider()
+        st.write('**Расстояние полёта**')
+        st.image(dist)
+        st.write('Самые частые полёты расстоянием до 1000 миль')
+        st.divider()
 
-    st.write('**Задержка вылета и прилёта**')
-    delays = Image.open('data/Delays.png')  # мб вынести наверх
-    st.image(delays)
-    st.divider()
+        st.write('**Задержка вылета и прилёта**')
+        st.image(delays)
+        st.write('Задержки вылета и прилёта крайне редки и не превышают трёх часов')
+        st.divider()
 
-    st.write('**Постполётные опросы** (14 вопросов по шкале от 0 до 5)')
-    scores = Image.open('data/Scores.png')  # мб вынести наверх
-    st.image(scores)
-    st.divider()
+        st.write('**Постполётные опросы** (14 вопросов по шкале от 0 до 5)')
+        st.image(scores)
+        st.write('Оценки по опросам сдвинуты влево, самая популярная оценка - 4')
+        st.write('Минимальных оценок достаточно мало')
+        st.divider()
 
-    # TODO добавить описания по 1-2 строки к каждому
+    with tab2:
+        st.write('Введите данные вашего пассажира:')
 
-with tab2:
-    st.write('Введите данные вашего пассажира:')
-    st.button('Сгенерировать случайно')
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            sex = st.selectbox('Пол', ['Женский', 'Мужской'])
+            travel_type = st.selectbox('Тип поездки', ['Личная', 'По работе'])
+        with col2:
+            age = st.slider('Возраст', min_value=0, max_value=100)
+            p_class = st.selectbox('Класс билета', ['Бизнес', 'Эко', 'Эко плюс'])
+        with col3:
+            loyalty = st.selectbox('Лояльность', ['Лояльный', 'Нелояльный'])
+            distance = st.slider('Расстояние полёта', min_value=100, max_value=3500)
+        st.divider()
+        col1, col2 = st.columns(2)
+        with col1:
+            dep_delay = st.slider('Задержка вылета, мин', min_value=0, max_value=500)
+        with col2:
+            arr_delay = st.slider('Задержка прилёта, мин', min_value=0, max_value=500)
+        st.divider()
 
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        sex = st.selectbox('Пол', ['Женский', 'Мужской'])
-        travel_type = st.selectbox('Тип поездки', ['Личная', 'По работе'])
-    with col2:
-        age = st.slider('Возраст', min_value=0, max_value=100)
-        p_class = st.selectbox('Класс билета', ['Бизнес', 'Эко', 'Эко плюс'])
-    with col3:
-        loyalty = st.selectbox('Лояльность', ['Лояльный', 'Нелояльный'])
-        distance = st.slider('Расстояние полёта', min_value=100, max_value=3500)
-    st.divider()
-    col1, col2 = st.columns(2)
-    with col1:
-        dep_delay = st.slider('Задержка вылета, мин', min_value=0, max_value=500)
-    with col2:
-        arr_delay = st.slider('Задержка прилёта, мин', min_value=0, max_value=500)
-    st.divider()
-    col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
-    with col1:
-        wifi = st.radio('Интернет на борту', [0, 1, 2, 3, 4, 5])
-        fun = st.radio('Развлечения на борту', [0, 1, 2, 3, 4, 5])
-    with col2:
-        time_conv = st.radio('Время вылета и прилёта', [0, 1, 2, 3, 4, 5])
-        onboard_service = st.radio('Обслуживание на посадке', [0, 1, 2, 3, 4, 5])
-    with col3:
-        booking = st.radio('Онлайн-бронирование', [0, 1, 2, 3, 4, 5])
-        leg = st.radio('Место в ногах', [0, 1, 2, 3, 4, 5])
-    with col4:
-        gate_loc = st.radio('Расположение выхода на посадку', [0, 1, 2, 3, 4, 5])
-        baggage = st.radio('Обращение с багажом', [0, 1, 2, 3, 4, 5])
-    with col5:
-        food = st.radio('Еда и напитки на борту', [0, 1, 2, 3, 4, 5])
-        checkin = st.radio('Регистрация на рейс', [0, 1, 2, 3, 4, 5])
-    with col6:
-        online_boarding = st.radio('Онлайн выбор места', [0, 1, 2, 3, 4, 5])
-        inflight_service = st.radio('Обслуживание на борту', [0, 1, 2, 3, 4, 5])
-    with col7:
-        seat = st.radio('Удобство сиденья', [0, 1, 2, 3, 4, 5])
-        cleanliness = st.radio('Чистота на борту', [0, 1, 2, 3, 4, 5])
+        st.write('Заполните опрос:')
+        col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+        with col1:
+            wifi = st.radio('Интернет на борту', [0, 1, 2, 3, 4, 5])
+            fun = st.radio('Развлечения на борту', [0, 1, 2, 3, 4, 5])
+        with col2:
+            time_conv = st.radio('Время вылета и прилёта', [0, 1, 2, 3, 4, 5])
+            onboard_service = st.radio('Обслуживание на посадке', [0, 1, 2, 3, 4, 5])
+        with col3:
+            booking = st.radio('Онлайн-бронирование', [0, 1, 2, 3, 4, 5])
+            leg = st.radio('Место в ногах', [0, 1, 2, 3, 4, 5])
+        with col4:
+            gate_loc = st.radio('Расположение выхода на посадку', [0, 1, 2, 3, 4, 5])
+            baggage = st.radio('Обращение с багажом', [0, 1, 2, 3, 4, 5])
+        with col5:
+            food = st.radio('Еда и напитки на борту', [0, 1, 2, 3, 4, 5])
+            checkin = st.radio('Регистрация на рейс', [0, 1, 2, 3, 4, 5])
+        with col6:
+            online_boarding = st.radio('Онлайн выбор места', [0, 1, 2, 3, 4, 5])
+            inflight_service = st.radio('Обслуживание на борту', [0, 1, 2, 3, 4, 5])
+        with col7:
+            seat = st.radio('Удобство сиденья', [0, 1, 2, 3, 4, 5])
+            cleanliness = st.radio('Чистота на борту', [0, 1, 2, 3, 4, 5])
 
-    col1, col2, col3 = st.columns(3)
-    if col2.button('Полетели!'):
-        with st.spinner('Считаем!'):
-            inputs = pack_input(sex, age, loyalty, distance, p_class, travel_type, dep_delay, arr_delay, wifi, fun,
-                                time_conv, onboard_service, booking, leg, gate_loc, baggage, food, checkin,
-                                online_boarding, inflight_service, seat, cleanliness)
-            scaled = pd.DataFrame(scaler.transform(inputs), columns=inputs.columns)
+        col1, col2, col3 = st.columns(3)
+        if col2.button('Полетели!'):
+            with st.spinner('Считаем!'):
+                inputs = pack_input(sex, age, loyalty, distance, p_class, travel_type, dep_delay, arr_delay, wifi, fun,
+                                    time_conv, onboard_service, booking, leg, gate_loc, baggage, food, checkin,
+                                    online_boarding, inflight_service, seat, cleanliness)
+                scaled = pd.DataFrame(scaler.transform(inputs), columns=inputs.columns)
 
-            pred, proba = predict_on_input(scaled)
-            if pred == 1:
-                st.success('Пассажир доволен! :thumbsup: :thumbsdown:')
-                with st.expander('Подробнее'):
-                    st.write(f'Вероятность этого: **`{round(max(proba[0]), 3)}`**')
-            elif pred == 0:
-                st.error('Пассажир не доволен :thumbsdown: :thumbsdown:')
-                with st.expander('Подробнее'):
-                    st.write(f'Вероятность этого: **`{round(max(proba[0]), 3)}`**')
-            else:
-                st.error('Что-то пошло не так...')
+                pred, proba = predict_on_input(scaled)
+                if pred == 1:
+                    st.success('Пассажир доволен! :thumbsup: :thumbsdown:')
+                    with st.expander('Подробнее'):
+                        st.write(f'Вероятность этого: **`{round(max(proba[0]), 3)}`**')
+                elif pred == 0:
+                    st.error('Пассажир не доволен :thumbsdown: :thumbsdown:')
+                    with st.expander('Подробнее'):
+                        st.write(f'Вероятность этого: **`{round(max(proba[0]), 3)}`**')
+                else:
+                    st.error('Что-то пошло не так...')
 
-    # TODO кнопка подставить случайные значения во все поля
-    # TODO украсить код, мб выделить функции
-    # TODO деплой
-    # TODO ридми
-    # TODO переписать анализ вклада факторов
+    with tab3:
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write('**Что важно для хорошего впечатления пассажира?**')
+            st.dataframe(get_importances(5, 'most').style.apply(highlight_weighs, axis=1))
+        with col2:
+            st.write('**А что практически не важно?**')
+            st.dataframe(get_importances(5, 'least').style.apply(highlight_weighs, axis=1))
 
-with tab3:
-    col1, col2 = st.columns(2)
-    with col1:
-        st.write('**Что важно для хорошего впечатления пассажира?**')
-        st.dataframe(get_importances(5, 'most').style.apply(highlight_weighs, axis=1))
-    with col2:
-        st.write('**А что практически не важно?**')
-        st.dataframe(get_importances(5, 'least').style.apply(highlight_weighs, axis=1))
+
+def load_page():
+    """ loads main page """
+
+    scaler, plane, age, cat, dist, delays, scores = preload_content()
+
+    st.set_page_config(layout="wide",
+                       page_title="Полёты и опросы",
+                       page_icon=':airplane:')
+
+    render_page(scaler, plane, age, cat, dist, delays, scores)
+
+
+if __name__ == "__main__":
+    load_page()
